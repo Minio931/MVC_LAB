0f4cbc710ff9a2abd47fa25af11076cb6986dfa8
Minio931 <d.chmielowski01@gmail.com>
2022-11-24 12:31:34 +0100

    Zadanie BlackBoard labolatorium 24-11-22

---
+++ MVC_LAB/Models/Person/PersonModel.cs
@@ -0,0 +1,48 @@
+﻿namespace MVC_LAB.Models.Person
+{
+    public class PersonModel
+    {
+        public PersonModel()
+        {
+            
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public GenderEnum Gender { get; set; }
+        public string City { get; set; }
+
+        public List<PersonModel> GetPeople(){
+            return new List<PersonModel>(){
+                new PersonModel()
+                {
+                 Id = 1,
+                 Name = "John",
+                 City = "Rzeszów",
+                 Gender = GenderEnum.Male,
+                 },
+                new PersonModel()
+                {
+                    Id = 2,
+                    Name = "Anna",
+                    City = "Kraków",
+                    Gender = GenderEnum.Female,
+                },
+                new PersonModel()
+                {
+                    Id = 3,
+                    Name = "Krzysztof",
+                    City = "Warszawa",
+                    Gender = GenderEnum.Male,
+                },
+                new PersonModel()
+                {
+                    Id = 4,
+                    Name = "Katarzyna",
+                    City = "Wrocław",
+                    Gender = GenderEnum.Female,
+                }
+                };
+        }
+    }
+}
